cac0e5039ea54638f2ebca128832f0c52a391f0f
Tobias Schmidt <tobias2.schmidt@ukr.de>
2023-11-09 17:17:31 +0100

    Initial commit

---
+++ cmd_kernel.py
@@ -0,0 +1,28 @@
+from ipykernel.kernelbase import Kernel
+from ipykernel.kernelapp import IPKernelApp
+import subprocess as sp
+
+class MyKernel(Kernel):
+    implementation = 'tosc_kernel'
+    implementation_version = "1.0.0"
+    banner = "cmd"
+    language_info = {'name': 'python', 'mimetype': 'text/x-python', 'file_extension': '.py'}
+
+    def do_execute(self, code, silent, store_history=True,
+                   user_expressions=None, allow_stdin=False):
+        output = sp.run(["cmd.exe", "/c", code], capture_output=True).stdout.decode('utf-8')
+        stream_content = {'name': 'stdout', 'text': output}
+        self.send_response(self.iopub_socket, 'stream', stream_content)
+        return {
+            'status': 'ok',
+            'execution_count': self.execution_count,
+            'payload': [],
+            'user_expressions': {}
+        }
+
+
+if __name__ == '__main__':
+    print("sdf")
+    with open("C:/Users/tobi/log.txt", "w") as f:
+        f.write("test")
+    IPKernelApp.launch_instance(kernel_class=MyKernel)
